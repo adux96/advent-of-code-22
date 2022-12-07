8d84ff674f851d60b6112bebcd3aa78bdb5c08fa
adux96 <119453358+adux96@users.noreply.github.com>
2022-12-07 17:15:03 -0600

    Update day_2.js
    Change the functions to output points into objects with key-value pairs.

--- day_2.js
+++ day_2.js
@@ -7,40 +7,29 @@ function syncReadFile(filename) {
     return arr;
 }
 
-// Function to determine if I win
-function winLoseDraw(self, enemy) {
-    if ((self === 'X' & enemy === 'A') || (self === 'Y' & enemy === 'B') || (self === 'Z' & enemy === 'C')) {
-        return 3;
-    } else if ((self === 'X' & enemy === 'C') || (self === 'Y' & enemy === 'A') || (self === 'Z' & enemy === 'B')) {
-        return 6;
-    } else {
-        return 0;
-    }
-}
-
-//Function to determine how many points for what I played
-function symbolPoints(sym) {
-    if (sym === 'X') {
-        return 1;
-    } else if (sym === 'Y') {
-        return 2;
-    } else {
-        return 3;
-    }
-}
-
 // Create array
 const arr = syncReadFile(__dirname + '/day_2_data.txt');
 let totalScore = 0;
-let opponent = '';
-let self = '';
+const symbols = {
+    'X': 1,
+    'Y': 2,
+    'Z': 3
+};
+const winOrLose = {
+    'A X': 3,
+    'B Y': 3,
+    'C Z': 3,
+    'C X': 6,
+    'A Y': 6,
+    'B Z': 6,
+    'A Z': 0,
+    'B X': 0,
+    'C Y': 0
+};
 
 // Loop through array, add up total score
 for (let i = 0; i < arr.length; i++) {
-    self = arr[i][2];
-    opponent = arr[i][0];
-    totalScore += winLoseDraw(self, opponent) + symbolPoints(self)
+    totalScore += winOrLose[arr[i]] + symbols[arr[i][2]];
 }
 
 console.log("totalScore: " + totalScore);
-
